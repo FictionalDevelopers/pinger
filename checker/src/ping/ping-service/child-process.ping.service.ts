898a2cda052d57e08b9e74da6467ddbf5f9a63c1
dmitry.honcahruk <dmitry.honcahruk@gmail.com>
2022-12-02 10:42:41 +0200

    debug

--- checker/src/ping/ping-service/child-process.ping.service.ts
+++ checker/src/ping/ping-service/child-process.ping.service.ts
@@ -4,35 +4,41 @@ import { IPAddress } from 'src/types/ip-address';
 import { TargetStatus } from './entities/target-status';
 import { PingService } from './ping.service';
 
+const rejectAfter = (time: number, promise: Promise<any>) =>
+  Promise.race([promise, new Promise((_, reject) => setTimeout(reject, time))]);
+
 @Injectable()
 export class ChildProcessPingService implements PingService {
   async pingIp(ip: IPAddress): Promise<TargetStatus> {
-    return new Promise((resolve, reject) => {
-      let result = '';
+    return rejectAfter(
+      100,
+      new Promise((resolve, reject) => {
+        let result = '';
 
-      const ping = spawn('ping', ['-W', '100', '-c', '1', ip.address]);
+        const ping = spawn('ping', ['-W', '100', '-c', '1', ip.address]);
 
-      ping.once('error', reject);
+        ping.once('error', reject);
 
-      ping.once('close', () => {
-        console.log(
-          '[RESULT]',
-          result,
-          /1\s+(?:packets )?received/.test(result)
-            ? TargetStatus.Online
-            : TargetStatus.Offline,
-        );
+        ping.once('close', () => {
+          console.log(
+            '[RESULT]',
+            result,
+            /1\s+(?:packets )?received/.test(result)
+              ? TargetStatus.Online
+              : TargetStatus.Offline,
+          );
 
-        return resolve(
-          /1\s+(?:packets )?received/.test(result)
-            ? TargetStatus.Online
-            : TargetStatus.Offline,
-        );
-      });
+          return resolve(
+            /1\s+(?:packets )?received/.test(result)
+              ? TargetStatus.Online
+              : TargetStatus.Offline,
+          );
+        });
 
-      ping.stdout.on('data', (chunk: Buffer) => {
-        result += `\n${chunk.toString('utf-8')}`;
-      });
-    });
+        ping.stdout.on('data', (chunk: Buffer) => {
+          result += `\n${chunk.toString('utf-8')}`;
+        });
+      }),
+    );
   }
 }

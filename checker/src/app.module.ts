ea327217e305532a85c646e11c37058fc255245e
Dmitry Honcharuk <dmitry.honcharuk@gmail.com>
2022-12-02 10:55:29 +0200

    Configured deploy (#1)
    * Configured deploy
    * Fixed issues

--- checker/src/app.module.ts
+++ checker/src/app.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
-import { DummyPingEffectNotificationService } from './ping/ping-effect-handler/ping-effect-notification.service';
+import { DummyPingEffectNotificationService } from './ping/ping-effect-notification/ping-effect-notification.service';
 import { DummyPingHistoryRepository } from './ping/ping-history/ping-history.repository';
 import { DummyPingHistoryService } from './ping/ping-history/ping-history.service';
 import { PingController } from './ping/ping.controller';
 import { RepositoryProvider } from './provider-tokens/repository-provider-tokens';
 import { ServiceProvider } from './provider-tokens/service-provider-tokens';
 import { ChildProcessPingService } from './ping/ping-service/child-process.ping.service';
+import { HealthzController } from './healthz/healthz.controller';
 
 @Module({
   imports: [],
-  controllers: [PingController],
+  controllers: [PingController, HealthzController],
   providers: [
     {
       provide: ServiceProvider.Ping,
--- checker/src/ping/ping-service/child-process.ping.service.ts
+++ checker/src/ping/ping-service/child-process.ping.service.ts
@@ -3,31 +3,32 @@ import { spawn } from 'child_process';
 import { IPAddress } from 'src/types/ip-address';
 import { TargetStatus } from './entities/target-status';
 import { PingService } from './ping.service';
+import { rejectAfter } from '../../utils/promise/reject-after';
 
 @Injectable()
 export class ChildProcessPingService implements PingService {
   async pingIp(ip: IPAddress): Promise<TargetStatus> {
-    return new Promise((resolve, reject) => {
-      let result = '';
+    return rejectAfter(
+      100,
+      new Promise((resolve, reject) => {
+        let result = '';
 
-      const ping = spawn('ping', ['-W', '100', '-c', '1', ip.address]);
+        const ping = spawn('ping', ['-W', '100', '-c', '1', ip.address]);
 
-      ping.once('error', reject);
+        ping.once('error', reject);
 
-      ping.once('close', () => {
-        if (result.includes('1 packets received')) {
-        }
+        ping.once('close', () => {
+          return resolve(
+            /1\s+(?:packets )?received/.test(result)
+              ? TargetStatus.Online
+              : TargetStatus.Offline,
+          );
+        });
 
-        return resolve(
-          result.includes('1 packets received')
-            ? TargetStatus.Online
-            : TargetStatus.Offline,
-        );
-      });
-
-      ping.stdout.on('data', (chunk: Buffer) => {
-        result += `\n${chunk.toString('utf-8')}`;
-      });
-    });
+        ping.stdout.on('data', (chunk: Buffer) => {
+          result += `\n${chunk.toString('utf-8')}`;
+        });
+      }),
+    ).catch(() => TargetStatus.Offline);
   }
 }
